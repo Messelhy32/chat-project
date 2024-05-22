022cf8500be7417569838d58cf3c41031a9bd89e
abdelrahman <messelhy32@gmail.com>
2024-05-22 10:56:02 +0300

    deploy process

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
@@ -8,7 +9,7 @@ import MongoDBConfig from "./db/MongoDBConfig.js";
 import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -18,9 +19,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   MongoDBConfig();
