ca76424c1aa1634f56bfa73f5000c43beb183a2a
abdelrahman <messelhy32@gmail.com>
2024-05-22 11:16:14 +0300

    fix signup

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -17,7 +17,6 @@ export const signup = async (req, res) => {
     //https://avatar-placeholder.iran.liara.run/
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
-    messelhy7;
     const newUser = new User({
       fullName,
       username,
